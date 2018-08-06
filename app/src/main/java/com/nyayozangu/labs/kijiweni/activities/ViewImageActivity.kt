013c65df65893616121ca4b4db885616c322af2d
Sean <paulsean@nyayozangu.com>
2018-08-06 20:34:55 +0300

    v0.0.12 toolbar changes: - added toolbar to view image activity

--- app/src/main/java/com/nyayozangu/labs/kijiweni/activities/ViewImageActivity.kt
+++ app/src/main/java/com/nyayozangu/labs/kijiweni/activities/ViewImageActivity.kt
@@ -2,25 +2,24 @@ package com.nyayozangu.labs.kijiweni.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.view.View
 import com.bumptech.glide.Glide
 import com.nyayozangu.labs.kijiweni.R
 import com.nyayozangu.labs.kijiweni.helpers.Common
 import com.nyayozangu.labs.kijiweni.helpers.IMAGE_URL
 import kotlinx.android.synthetic.main.activity_view_image.*
 
-
 class ViewImageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_view_image)
+        setSupportActionBar(viewImageToolbar)
         val actionBar = supportActionBar
         actionBar?.setDisplayHomeAsUpEnabled(true)
         actionBar?.title = getString(R.string.app_name)
-        //todo replace tool bar and add menu
+        viewImageToolbar.setNavigationOnClickListener {
+            finish()
+        }
         handleIntent()
     }
 
